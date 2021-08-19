d75ef97f63b82adecd6148223b773890dd6ee416
BrunoTruber <brunotruber45@gmail.com>
2021-08-19 00:00:41 -0300

    ok

--- bluejs/aula2-codelab/index.js
+++ bluejs/aula2-codelab/index.js
@@ -1,1 +1,18 @@
 const prompt = require("prompt-sync")();
+
+/*const lista = [];
+
+do {
+    let num = +prompt('numero: ');
+    const exist = lista.find(item => item == num)
+    if (exist == undefined){
+        lista.push(num)
+    } else {
+        console.log('Esse numero ja existe')
+    }
+    
+    
+    var resp = prompt('deseja continur? [S/N] ').toUpperCase()[0];
+} while (resp == 'S');
+
+console.log(lista)*/

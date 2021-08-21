e883b72f480e1d7f91159fb174e8ba24caf7a08b
BrunoTruber <brunotruber45@gmail.com>
2021-08-21 15:23:47 -0300

    express

--- bluejs/revisao-final-d-s/index.js
+++ bluejs/revisao-final-d-s/index.js
@@ -1,6 +1,16 @@
-const http = require("http");
+// const http = require("http");
 
-http.createServer((req, res) => {
-    res.end("Hello Wold, okkk");
-   }).listen(3000);
-console.log("ok");
+// http.createServer((req, res) => {
+//     res.send("Hello Wold, okkk");
+//    }).listen(3000);
+
+// console.log("ok");
+
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World Express');
+});
+ 
+app.listen(3000);

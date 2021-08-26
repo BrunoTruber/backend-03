86f4b7f170bd1ff8d09e37e9f3d8b62a05062e09
BrunoTruber <brunotruber45@gmail.com>
2021-08-25 23:08:22 -0300

    new

---
+++ bluejs/aula7-codelab-projeto/crud-mongo-db/database/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/db_filmes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+});
+
+module.exports = mongoose;
--- bluejs/aula7-codelab-projeto/crud-mongo-db/index.js
+++ bluejs/aula7-codelab-projeto/crud-mongo-db/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const filmeSchema = require('./models/filme');
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send({ info: 'Hello mongo db'})
+    res.send({ info: 'Hello mongo db'});
 });
+
+app.get('/filmes', (req, res) => {
+    const filmes = await filmeSchema.find();
+    res.send(filmes);
+})
 
 app.listen(port, () => {
     console.log(`server cool in http://localhost${port}`)
